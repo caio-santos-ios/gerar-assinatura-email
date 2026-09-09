7797bf9e33c3161d9ccccca031051cf8295ec942
Caio Santos <caiosantos.dev@outlook.com>
2026-09-09 09:29:33 -0300

    fix: ajustes nos icones

--- src/jotai/networks.jotai.ts
+++ src/jotai/networks.jotai.ts
@@ -7,15 +7,18 @@ export type TNetworks = {
 
 const BASE = 'https://img.icons8.com/ios-filled/50'
 
-const blueskyOGA     = 'https://api.iconify.design/simple-icons:bluesky.svg?color=%232D6A2D'
-const linktreeOGA    = 'https://api.iconify.design/simple-icons:linktree.svg?color=%232D6A2D'
+const toPng = (svgUrl: string, size = 96) =>
+  `https://wsrv.nl/?url=${encodeURIComponent(svgUrl)}&output=png&w=${size}`
 
-const blueskyJustice  = 'https://api.iconify.design/simple-icons:bluesky.svg?color=%232A9D8F'
-const linktreeJustice = 'https://api.iconify.design/simple-icons:linktree.svg?color=%232A9D8F'
+const blueskyOGA     = toPng('https://api.iconify.design/simple-icons:bluesky.svg?color=%232D6A2D')
+const linktreeOGA    = toPng('https://api.iconify.design/simple-icons:linktree.svg?color=%232D6A2D')
 
-// const blueskyRaizes  = 'https://api.iconify.design/simple-icons:bluesky.svg?color=%23E8A020'
-const siteRaizes = 'https://api.iconify.design/mdi:web.svg?color=%23E8A020'
-const linktreeRaizes = 'https://api.iconify.design/simple-icons:linktree.svg?color=%23E8A020'
+const blueskyJustice  = toPng('https://api.iconify.design/simple-icons:bluesky.svg?color=%232A9D8F')
+const linktreeJustice = toPng('https://api.iconify.design/simple-icons:linktree.svg?color=%232A9D8F')
+
+// const blueskyRaizes  = toPng('https://api.iconify.design/simple-icons:bluesky.svg?color=%23E8A020')
+const siteRaizes = toPng('https://api.iconify.design/mdi:web.svg?color=%23E8A020')
+const linktreeRaizes = toPng('https://api.iconify.design/simple-icons:linktree.svg?color=%23E8A020')
 
 export const networksOGAAtom = atom<TNetworks[]>([
   { uri: 'https://www.instagram.com/oga.web',                      image: `${BASE}/2D6A2D/instagram-new.png` },
